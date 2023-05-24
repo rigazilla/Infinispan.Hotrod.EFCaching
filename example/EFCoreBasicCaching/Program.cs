ac71c73977852679956911edce5208cdbe639fbf
rigazilla <vrigamon@redhat.com>
2023-05-24 16:41:19 +0200

    moved example to new folder. added items to order

--- example/EFCoreBasicCaching/Program.cs
+++ example/EFCoreBasicCaching/Program.cs
@@ -1,6 +1,4 @@
-﻿using System.Reflection.Metadata;
-
-namespace EFCoreBasicCaching
+﻿namespace EFCoreBasicCaching
 {
     internal class Program
     {
@@ -11,17 +9,21 @@ namespace EFCoreBasicCaching
             // Note: This sample requires the database to be created before running.
             Console.WriteLine($"Database path: {db.DbPath}.");
             
-            // Ensure the schema is created
+            // Ensure the schema is created from scratch
+            db.Database.EnsureDeleted();
             db.Database.EnsureCreated();
             // Create a menu
-            Console.WriteLine("Inserting a new Menu");
+            Console.WriteLine("Inserting a new detached Menu item");
             db.Add(new Menu {MenuId= 1, MenuName = "Chilly Chicken", 
                 MenuDescription = "Spicy chicken, fried with bell peppers in hot garlic sauce." });
             db.SaveChanges();
 
             // Create a Order
-            Console.WriteLine("Inserting a new Order");
-            db.Add(new Order { OrderDate = DateTime.Now,  TotalPrice = 20 });
+            Console.WriteLine("Inserting a new Order with an entry");
+            var items = new List<Menu>();
+            items.Add (new Menu {MenuId= 2, MenuName = "Tomato Chicken", 
+                MenuDescription = "Chicken, fried fresh tomatoes." });
+            db.Add(new Order { OrderDate = DateTime.Now,  TotalPrice = 20, MenuItems =  items});
             db.SaveChanges();
             Console.WriteLine("Data inserted successfully.");
 
